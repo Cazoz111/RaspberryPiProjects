7b8d341257755b113e72797e3ba74fe0e64ba2be
Cazoz111 <77897180+Cazoz111@users.noreply.github.com>
2021-01-30 13:49:34 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,73 +1,8 @@
-from flask import Flask, render_template, current_app as app
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    
-    return render_template('rainbow.html')
-    
-
-@app.route('/red')
-def red():
-    name = "red"
-    background = "#FF0000"
-
-    return render_template('rainbow.html', name = name, background = background)
-
-@app.route('/orange')
-def orange():
-    name = "orange"
-    background = "#FF8C00"
-
-    return render_template('rainbow.html', name = name, background = background)
+from flask import Flask, render_template, request, json, jsonify, current_app as app,
+import requests, from datetime import date, import os 
 
 
 
-
-@app.route('/yellow')
-def yellow():
-    name = "yellow"
-    background = "#FFFF00"
-
-    return render_template('rainbow.html', name = name, background = background)
-
-
-@app.route('/green')
-def green():
-    name = "green"
-    background = "#008000"
-
-    return render_template('rainbow.html', name = name, background = background)
-
-
-@app.route('/blue')
-def blue():
-    name = "blue"
-    background = "#00008B"
-
-    return render_template('rainbow.html', name = name, background = background)
-
-@app.route('/indigo')
-def indigo():
-    name = "indigo"
-    background = "#4B0082"
-
-    return render_template('rainbow.html', name = name, background = background)
-
-@app.route('/violet')
-def violet():
-    name = "voilet"
-    background = "#4F2F4F"
-
-    return render_template('rainbow.html', name = name, background = background)
-
-@app.route('/friends')
-def friends():
-    friends = ["Isreal", "Jason", "Abdul", "Roman"]
-    return render_template('rainbow.html', friends = friends)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
-
