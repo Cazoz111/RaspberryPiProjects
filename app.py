4bb19e13390ca42a19bd313abb0ddee66302fc6b
Cazoz111 <77897180+Cazoz111@users.noreply.github.com>
2021-02-06 11:49:33 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template, request, json, jsonify, current_app as app,
-import requests, from datetime import date, import os 
+from flask import Flask, render_template, request, json, jsonify, current_app as app
+from datetime import date
+import os
+import requests
 
 
+
+app = Flask(__name__)
+
+@app.route('/brasil')
+def pic():
+        response = requests.get('https://date.nager.at/api/v2/publicholidays/2017/BR')
+        data = response.json()
+
+        return render_template('holiday.html', data=data)
 
 
 if __name__ == '__main__':
